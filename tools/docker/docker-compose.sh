6662e05e75134762570daf75197f24f0fe91dec9
Brian McVeigh <bcmcveigh@gmail.com>
2019-01-12 16:21:53 -0500

    Separated docker and docker-compose scripts.

---
+++ tools/docker/docker-compose.sh
@@ -0,0 +1,9 @@
+########################################
+# Aliases and logic for Docker Compose.
+#
+# @see https://www.docker.com/
+########################################
+
+alias dcu='docker-compose up'
+alias dcud='docker-compose up -d'
+alias dcd='docker-compose down'
--- tools/docker/docker.sh
+++ tools/docker/docker.sh
@@ -1,10 +1,8 @@
 ########################################
-# Aliases for Docker and Docker Compose.
+# Aliases for Docker.
 #
 # @see https://www.docker.com/
 ########################################
 
 alias d='docker $*'
-alias dcu='docker-compose up'
-alias dcud='docker-compose up -d'
-alias dcd='docker-compose down'
+
