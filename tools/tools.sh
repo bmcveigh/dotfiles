8cc9318e9fb2952ee03a39e98830ac9260bc1d7e
Brian McVeigh <bcmcveigh@gmail.com>
2019-01-12 17:44:05 -0500

    Fixed issue with autoloader.

--- tools/tools.sh
+++ tools/tools.sh
@@ -3,7 +3,7 @@ export TOOLS_ROOT=$DOTFILES_ROOT/tools;
 # Autoload in all of the sh automatically
 # instead of manually listing all the sh
 # files.
-for file in $(pwd)/tools/*/*.sh
+for file in $HOME/.dotfiles/tools/*/*.sh
 do
     source "$file";
 done
