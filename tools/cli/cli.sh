80b08b44f3a66ba67970da074c6ad1ac4f50bedb
Brian McVeigh <bcmcveigh@gmail.com>
2019-01-12 18:28:02 -0500

    Added alias for alias command.

---
+++ tools/cli/cli.sh
@@ -0,0 +1,6 @@
+# -------------------------------------------------------
+# Aliases and functions for Built in UNIX/Linux commands.
+# -------------------------------------------------------
+
+# View all aliases.
+alias a='alias'
