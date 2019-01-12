e9280bfc0270e6503ab5a87a3b63b83d0c2a03a1
Brian McVeigh <bcmcveigh@gmail.com>
2019-01-12 16:59:49 -0500

    Added more vagrant commands.

--- tools/vagrant/vagrant.sh
+++ tools/vagrant/vagrant.sh
@@ -4,7 +4,10 @@
 # @see https://www.vagrantup.com/
 ########################################
 
+alias v="vagrant"
 alias vu="vagrant up"
 alias vh="vagrant halt"
 alias vd="vagrant destroy"
+alias vp="vagrant provision"
+alias vr="vagrant reload"
 alias vs="vagrant ssh"
