d496fbdb3b141027e35de92e679f94a4c424df10
Brian McVeigh <bcmcveigh@gmail.com>
2019-01-12 19:33:22 -0500

    Move scripts into bash directory.

--- tools/bash/tools.sh
+++ tools/bash/tools.sh
@@ -3,7 +3,7 @@ export TOOLS_ROOT=$DOTFILES_ROOT/tools;
 # Autoload in all of the sh automatically
 # instead of manually listing all the sh
 # files.
-for file in $HOME/.dotfiles/tools/*/*.sh
+for file in $HOME/.dotfiles/tools/bash/*/*.sh
 do
     source "$file";
 done
