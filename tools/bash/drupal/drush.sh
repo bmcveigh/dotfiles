1815a1a5f5f351b2519d94afc6635fa8915feccb
Brian <bcmcveigh@gmail.com>
2019-09-17 13:48:06 -0400

    Added some aliases related to Drush.

---
+++ tools/bash/drupal/drush.sh
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------
+# Aliases and functions for Drush.
+# ---------------------------------------------------------------------
+
+# Aliases related to Docksal.
+alias fdr='fin drush'
+alias fdrs='fin drush status'
+alias fdrfu='fin drush fu'
+alias fdrfuy='fin drush -y fu'
