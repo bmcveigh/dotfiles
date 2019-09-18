1d50edd2e12aa078f6e51d1bcfcf51bec597fa9b
Brian <bcmcveigh@gmail.com>
2019-09-18 10:17:20 -0400

    Colors for bash prompt.

--- tools/bash/prompt/prompt.sh
+++ tools/bash/prompt/prompt.sh
@@ -6,3 +6,8 @@ parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ \[\033[01;33m\]"
+
+# Output colors for CLI ls command.
+# See: https://apple.stackexchange.com/questions/33677/how-can-i-configure-mac-terminal-to-have-color-ls-output
+export CLICOLOR=1
+export LSCOLORS="ExGxBxDxCxEgEdxbxgxcxd"
