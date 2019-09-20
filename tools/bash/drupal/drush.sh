c81f3b96f7faa9ed976f44b330a7385474242ad3
Brian McVeigh <bmcveigh@forumone.com>
2019-09-20 09:44:29 -0400

    Added lando drush aliases.

--- tools/bash/drupal/drush.sh
+++ tools/bash/drupal/drush.sh
@@ -4,6 +4,16 @@
 
 # Aliases related to Docksal.
 alias fdr='fin drush'
+alias fdrudb='fin drush updb'
+alias fdrudby='fin drush -y updb'
 alias fdrs='fin drush status'
 alias fdrfu='fin drush fu'
 alias fdrfuy='fin drush -y fu'
+
+# Aliases related to Lando.
+alias ldr='lando drush'
+alias ldrudb='lando drush updb'
+alias ldrudby='lando drush -y updb'
+alias ldrs='lando drush status'
+alias ldrfu='lando drush fu'
+alias ldrfuy='lando drush -y fu'
