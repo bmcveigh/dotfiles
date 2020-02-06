7f2885f3f35a5ed00be0e3661bb4decafd323335
Brian McVeigh <bcmcveigh@gmail.com>
2020-02-06 14:22:45 -0500

    Added aliases for Docksal.

---
+++ tools/bash/docksal/docksal.sh
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------------------
+# Aliases and functions for Docksal.
+# ---------------------------------------------------------------------
+
+alias fpsta='fin project start'
+alias fpsto='fin project stop'
+alias fpr='fin project restart'
