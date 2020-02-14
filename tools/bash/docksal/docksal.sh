bb60b8554251d8b720607520370b36d50088efd4
Brian McVeigh <bcmcveigh@gmail.com>
2020-02-14 10:12:16 -0500

    Added Docksal composer commands.

--- tools/bash/docksal/docksal.sh
+++ tools/bash/docksal/docksal.sh
@@ -5,3 +5,10 @@
 alias fpsta='fin project start'
 alias fpsto='fin project stop'
 alias fpr='fin project restart'
+
+# Docksal composer commands.
+alias finc='fin composer'
+alias finci='fin composer install'
+alias fincu='fin composer update'
+alias fincun='fin composer update nothing'
+alias fincr='fin composer require'
