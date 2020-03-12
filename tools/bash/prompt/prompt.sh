ff732b01a8d5653a3e68a99f77995579e89a454f
Brian McVeigh <bcmcveigh@gmail.com>
2020-03-11 20:19:46 -0400

    Added timestamp to bash prompt.

--- tools/bash/prompt/prompt.sh
+++ tools/bash/prompt/prompt.sh
@@ -5,7 +5,11 @@
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ \[\033[01;33m\]"
+
+display_time() {
+   date +"%T"
+}
+export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] @ \[\033[32m\]\\D{%T}\[\033[00m\] $ \[\033[01;33m\]"
 
 # Output colors for CLI ls command.
 # See: https://apple.stackexchange.com/questions/33677/how-can-i-configure-mac-terminal-to-have-color-ls-output
