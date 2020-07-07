82edc24e471f2c7cfdbc95bb3761f464b45f53ee
Brian McVeigh <bcmcveigh@gmail.com>
2020-07-07 10:25:40 -0400

    Modified bash prompt icon.

--- tools/bash/prompt/prompt.sh
+++ tools/bash/prompt/prompt.sh
@@ -9,7 +9,7 @@ parse_git_branch() {
 display_time() {
    date +"%T"
 }
-export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] @ \[\033[32m\]\\D{%T}\[\033[00m\]\n$ \[\033[01;33m\]"
+export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] @ \[\033[32m\]\\D{%T}\[\033[00m\]\n> \[\033[01;33m\]"
 
 # Output colors for CLI ls command.
 # See: https://apple.stackexchange.com/questions/33677/how-can-i-configure-mac-terminal-to-have-color-ls-output
