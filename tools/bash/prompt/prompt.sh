30152b1a8a26a9d670a27eac2e482cac1d58a3f5
Brian McVeigh <bcmcveigh@gmail.com>
2020-07-08 10:06:00 -0400

    Modified colors for bash prompt.

--- tools/bash/prompt/prompt.sh
+++ tools/bash/prompt/prompt.sh
@@ -2,6 +2,14 @@
 # Scripts for the bash prompt.
 #############################
 
+COLOR_RED="\033[0;31m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_GREEN="\033[0;32m"
+COLOR_OCHRE="\033[38;5;95m"
+COLOR_BLUE="\033[0;34m"
+COLOR_WHITE="\033[0;37m"
+COLOR_RESET="\033[0m"
+
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
@@ -9,7 +17,7 @@ parse_git_branch() {
 display_time() {
    date +"%T"
 }
-export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] @ \[\033[32m\]\\D{%T}\[\033[00m\]\n> \[\033[01;33m\]"
+export PS1="$(echo $COLOR_GREEN)\w$(echo $COLOR_YELLOW)\$(parse_git_branch)$(echo $COLOR_RESET) $(echo $COLOR_RED)@\$(echo $COLOR_RESET) $(echo $COLOR_BLUE)\\D{%T}$(echo $COLOR_RESET)\n> $(echo $COLOR_YELLOW)"
 
 # Output colors for CLI ls command.
 # See: https://apple.stackexchange.com/questions/33677/how-can-i-configure-mac-terminal-to-have-color-ls-output
