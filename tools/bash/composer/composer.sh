67ee78c5a3501ff8a7068a5ba3232d796d4d587a
Brian McVeigh <bcmcveigh@gmail.com>
2020-10-19 10:26:20 -0400

    Fixed composer memory_limit issue.

--- tools/bash/composer/composer.sh
+++ tools/bash/composer/composer.sh
@@ -4,6 +4,6 @@
 # @see https://getcomposer.org/
 # ------------------------------------
 
-alias {composer,c}="php -d memory-limit=-1 /usr/local/bin/composer"
+alias {composer,c}="php -d memory_limit=-1 /usr/local/bin/composer"
 alias cr="composer require"
 alias ci="composer install"
