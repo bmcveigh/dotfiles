aacce6820f6fdf827309f6ec89da44a72574d8cb
Brian McVeigh <bcmcveigh@gmail.com>
2026-03-04 11:42:44 -0500

    Add f and l aliases.
    The f alias uses fzf and l uses eza.

--- tools/bash/cli/cli.sh
+++ tools/bash/cli/cli.sh
@@ -5,5 +5,8 @@
 # View all aliases.
 alias a='alias'
 
+# Find and open files using FZF.
+alias f='file=$(fzf --style full --preview "fzf-preview.sh {}") && [ -n "$file" ] && nvim "$file"'
+
 # List all files in a directory, including private files.
-alias l='ls -lahG'
+alias l='eza -l --icons --long --all'
